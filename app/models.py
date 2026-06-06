6d8ca7060dc7d6e5e5054e8f0af59b98521e0b43
alexdubskiycraft <alex.dubskiy.craft@gmail.com>
2026-06-06 14:01:20 +0300

    version 1.1

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Teacher(models.Model):
     firstname = models.CharField(max_length=150)
     lastname = models.CharField(max_length=150)
     middlename = models.CharField(max_length=150)
-    subject = models.ForeignKey(Subject, related_name='subjects', on_delete=models.CASCADE) 
+    subject = models.ForeignKey(Subject, related_name='teachers', on_delete=models.CASCADE) 
 
 class Class(models.Model):
     name = models.CharField(max_length=50)
@@ -16,17 +16,17 @@ class Student(models.Model):
     firstname = models.CharField(max_length=150)
     lastname = models.CharField(max_length=150)
     middlename = models.CharField(max_length=150)
-    age = models.IntegerField(max_length=2)
-    classes = models.ForeignKey(Class, related_name='classes', on_delete=models.CASCADE)
+    age = models.IntegerField()
+    classes = models.ForeignKey(Class, related_name='students', on_delete=models.CASCADE)
 
 class Schedule(models.Model):
-    lesson_number = models.IntegerField(max_length=2)
-    day_number = models.IntegerField(max_length=2)    
-    subject = models.ForeignKey(Subject, related_name='subjects', on_delete=models.CASCADE)
-    classes = models.ForeignKey(Class, related_name='classes', on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, related_name='teachers', on_delete=models.CASCADE)
+    lesson_number = models.IntegerField()
+    day_number = models.IntegerField()    
+    subject = models.ForeignKey(Subject, related_name='schedules', on_delete=models.CASCADE)
+    classes = models.ForeignKey(Class, related_name='schedules', on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, related_name='schedules', on_delete=models.CASCADE)
 
 class Grade(models.Model): 
-    value = models.IntegerField(max_length=2)
-    subject = models.ForeignKey(Subject, related_name='subjects', on_delete=models.CASCADE)   
-    student = models.ForeignKey(Student, related_name='students', on_delete=models.CASCADE)
+    value = models.IntegerField()
+    subject = models.ForeignKey(Subject, related_name='grades', on_delete=models.CASCADE)   
+    student = models.ForeignKey(Student, related_name='grades', on_delete=models.CASCADE)
---
+++ django_setup.py
@@ -0,0 +1,6 @@
+import sys
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "crud_shool_schedule.settings")
+django.setup()
